1c9a6202c5a17a3c826d338b551a949dba04129d
silvershadow09012000 <silvershadow09012000@gmail.com>
2026-07-17 17:02:55 +0530

    Update payment.js

--- iycncon-payment-pages/js/payment.js
+++ iycncon-payment-pages/js/payment.js
@@ -1,8 +1,19 @@
-function launch(app, params) {
+function launch() {
 
-    const uri = "upi://pay?" + params;
+    const params = new URLSearchParams({
+        pa: "apps290@oksbi",
+        pn: "APOORVA DODDAMANI",
+        am: "5",
+        cu: "INR",
+        tn: "Faculty Registration - IYCNCON 2026",
+        tr: "FAC" + Date.now(),
+        mc: "0000"
+    });
+
+    const uri = "upi://pay?" + params.toString();
 
     alert(uri);
 
     window.location.href = uri;
+
 }
