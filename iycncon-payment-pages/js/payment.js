5e038a295dd02a58a1c0c656b0c08ece75ff137a
silvershadow09012000 <silvershadow09012000@gmail.com>
2026-07-17 17:33:36 +0530

    Update payment.js

--- iycncon-payment-pages/js/payment.js
+++ iycncon-payment-pages/js/payment.js
@@ -1,19 +1,43 @@
-function launch() {
+function launch(app) {
+
+    let packageName = "";
+
+    switch(app) {
+
+        case "gpay":
+            packageName = "com.google.android.apps.nbu.paisa.user";
+            break;
+
+        case "phonepe":
+            packageName = "com.phonepe.app";
+            break;
+
+        case "bhim":
+            packageName = "in.org.npci.upiapp";
+            break;
+
+        default:
+            packageName = "";
+    }
 
     const params = new URLSearchParams({
         pa: "apps290@oksbi",
         pn: "APOORVA DODDAMANI",
-        am: "5",
+        am: "3",
         cu: "INR",
         tn: "Faculty Registration - IYCNCON 2026",
-        tr: "FAC" + Date.now(),
-        mc: "0000"
+        tr: "FAC" + Date.now()
     });
 
-    const uri = "upi://pay?" + params.toString();
+    const intent =
+        "intent://pay?" +
+        params.toString() +
+        "#Intent;" +
+        "scheme=upi;" +
+        "package=" + packageName + ";" +
+        "end";
 
-    alert(uri);
+    alert(intent);
 
-    window.location.href = uri;
-
+    window.location.href = intent;
 }
