335008963ad0d944798039ae9d14ca25c3a1bc3b
silvershadow09012000 <silvershadow09012000@gmail.com>
2026-07-17 15:23:35 +0530

    Update payment.js

--- iycncon-payment-pages/js/payment.js
+++ iycncon-payment-pages/js/payment.js
@@ -1,23 +1,8 @@
 function launch(app, params) {
 
-    const upiUrl = 'upi://pay?' + params;
+    const uri = "upi://pay?" + params;
 
-    if (app === 'phonepe') {
-        window.location.href = 'phonepe://pay?' + params;
-        setTimeout(() => {
-            window.location.href = upiUrl;
-        }, 800);
-    }
+    alert(uri);
 
-    else if (app === 'gpay') {
-        window.location.href = 'tez://upi/pay?' + params;
-        setTimeout(() => {
-            window.location.href = upiUrl;
-        }, 800);
-    }
-
-    else {
-        window.location.href = upiUrl;
-    }
-
+    window.location.href = uri;
 }
